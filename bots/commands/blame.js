7d518e57b3567ce2e612668d2cf7a3bfeb2091de
Glitch (mse-so-se-chatbot) <none>
2019-05-22 11:05:30 +0000

    excluded some users

--- bots/commands/blame.js
+++ bots/commands/blame.js
@@ -1,8 +1,26 @@
 const util = require('../util.js');
 const parser = require('../../utils/htmlparser.js');
 
+const allExcludedUsers = {
+        "https://chat.meta.stackexchange.com" : {
+          // the den
+          "721": {
+             "377214": true, // sonic
+          },
+          // sandbox
+          "1196": {
+             "152859": true, // shadowwizard
+             "158100": true, // rene
+          }
+        }
+      };
+
+
 function Blame(bot) {
-  var silent = false, ttw;
+  var silent = false, ttw;  
+  console.log('blame',bot.getHostname());
+  var exclusions = allExcludedUsers[bot.getHostname()] || {};
+  console.log('blame excl',exclusions);
   
   function getRandomArbitrary(min, max) {
     return Math.floor(Math.random() * (max - min)) + min;
@@ -17,16 +35,25 @@ function Blame(bot) {
     ttw: function() {return ttw},
     events: [1, 2],
     next: function (ce, arg) {
-      var u, usr, nextTime;
+      var u, usr, nextTime, excludedUsers = exclusions[ce.room_id] || {};
+      console.log('blame next ', excludedUsers, ce, ce.room_id);
       if (!silent) {
         var seenUsers = bot.seenUsers(), userlist = [];
         for (u in seenUsers) {
           if (seenUsers.hasOwnProperty(u)) {
             usr = seenUsers[u];
+            console.log('balme seen ', usr.id, excludedUsers[usr.id]);
             if (usr.name && 
                 usr.cnt && 
                 usr.is_moderator === false) {
-              userlist.push( { name: usr.name });
+              if (excludedUsers[usr.id] === true) {
+                console.log('explicitly excluded ', usr.id, excludedUsers[usr.id]);
+              } else {
+                console.log('added ', usr.id, excludedUsers[usr.id]);
+                userlist.push( { name: usr.name });
+              }
+            } else {
+              console.log('skipped ', usr.id, excludedUsers[usr.id]);
             }
           }
         }
